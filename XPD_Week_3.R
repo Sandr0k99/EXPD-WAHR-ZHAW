d130606d8ddf1355fd9069235530ffb205a583ff
Sandr0k99 <s.kostenzer@hotmail.com>
2024-12-20 21:18:21 +0100

    Update Week 3

--- XPD_Week_3.R
+++ XPD_Week_3.R
@@ -20,17 +20,33 @@ library(readxl)
 table.survey <- read_excel("./Data/Fragebogen_ExpD_hs24.xlsx")
 
 num_rows <- nrow(table.survey)
-table.survey$Koerpergroesse[23]
-table.survey$`Statistische Vorkenntnisse`[13]
-table.survey$Ausbildung[15]
-table.survey[5,]$Postleitzahl
-table.survey[18,c("Augenfarbe","Ausbildung")]
-table.survey[c(6,19),c("Sport","Anreisezeit an die ZHAW")]
-table.survey[c(21:23),c("Haarfarbe","Augenfarbe")]
-table.survey[c(1:10),"Geschlecht"]
-table.survey[c((num_rows-10):num_rows), "Schlafzimmer"]
-sum(as.integer(table.survey$Handspanne),na.rm = TRUE) / (num_rows -1)
-sum(as.integer(table.survey$Koerpergroesse)) / num_rows
+table.survey$Schuhgrösse <- as.numeric(table.survey$Schuhgrösse)
 
+table.survey$Koerpergroesse[23]
+"178"
+table.survey$`Statistische Vorkenntnisse`[13]
+"gering"
+table.survey$Ausbildung[15]
+"Kaufmann"
+table.survey[5,]$Postleitzahl
+"8472"
+table.survey[18,c("Augenfarbe","Ausbildung")]
+"blau,Buchhändlerin EFZ"
+table.survey[c(6,19),c("Sport","Anreisezeit an die ZHAW")]
+"6     90"                      
+"6     40"
+table.survey[c(21:23),c("Haarfarbe","Augenfarbe")]
+"braun     Braun"
+"schwarz   Braun"     
+"blond     blau"
+table.survey[c(1:10),"Geschlecht"]
+"9 x männlich, 1 x weiblich"
+table.survey[c((num_rows-10):num_rows), "Schlafzimmer"]
+"1 x Parterre"
+sum(as.integer(table.survey$Handspanne),na.rm = TRUE) / (num_rows -1)
+"19"
+sum(as.integer(table.survey$Koerpergroesse)) / num_rows
+"173.7"
+median(table.survey[1:num_rows != 16, "Schuhgrösse"], na.rm = T)
 
 
